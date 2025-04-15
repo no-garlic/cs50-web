7d6b261fa8959d72ebad88638f6cf69cf1c5da88
Michael Petrou <mpetrou74@hotmail.com>
2025-04-15 09:45:17 +0930

    Refactor admin registration to use custom ModelAdmin classes for User and Category

--- code/4-commerce/auctions/admin.py
+++ code/4-commerce/auctions/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "email")
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Auction)
 admin.site.register(Bid)
 admin.site.register(Comment)
 admin.site.register(Watchlist)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
