4cef0899ee16f2a2198a9a51640476b9f614e9af
Michael Petrou <mpetrou74@hotmail.com>
2025-05-06 11:29:24 +0930

    Refactor code structure for improved readability and maintainability

---
+++ code/7-network/network/static/network/create.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('textarea').focus();
+    document.querySelector('textarea').setSelectionRange(0, 0);
+
+});
