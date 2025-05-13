efb5bd9fc41ebcbfdeaa4723e79875ca6fcc3972
Michael Petrou <mpetrou74@hotmail.com>
2025-05-13 17:26:03 +0930

    changed layout

--- code/8-capstone/quizly/static/quizly/create.js
+++ code/8-capstone/quizly/static/quizly/create.js
@@ -3,12 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const showFormButton = document.getElementById('show-question-form');
     const questionFormContainer = document.getElementById('question-form-container');
     const buttonContainer = document.querySelector('.add-question-button-container');
+    const saveQuestionButtonContainer = document.getElementById('save-question-button-container');
     
     // Add click event listener to the button
     if (showFormButton && questionFormContainer) {
         showFormButton.addEventListener('click', function() {
             // Show the form container
             questionFormContainer.style.display = 'flex';
+            
+            // Show the save button container
+            saveQuestionButtonContainer.style.display = 'flex';
             
             // Hide the button container
             buttonContainer.style.display = 'none';
