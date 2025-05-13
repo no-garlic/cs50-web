b6fdf5e2de0345f551d52a562fcc98549e9b24d6
Michael Petrou <mpetrou74@hotmail.com>
2025-05-13 16:45:34 +0930

    got adding a quiz and adding questions to work.

--- code/8-capstone/quizly/views/create.py
+++ code/8-capstone/quizly/views/create.py
@@ -14,24 +14,64 @@ from ..models import *
 @login_required
 def create(request):
     if request.method == "POST":
-        name = request.POST.get("name")
-        description = request.POST.get("description")
-        category_id = request.POST.get("category")
-        category = Category.objects.get(id=category_id)
+        form_action = request.POST.get("form_action")
 
-        # TODO: Create the embedding
-        embedding = None
+        if form_action == "save_quiz":
+            name = request.POST.get("name")
+            description = request.POST.get("description")
+            category_id = request.POST.get("category")
+            category = Category.objects.get(id=category_id)
 
-        # Create the quiz
-        quiz = Quiz.objects.create(
-            name=name,
-            description=description,
-            category=category,
-            created_by=request.user,
-            embedding=embedding,
-        )
+            # TODO: Create the embedding
+            embedding = None
 
-        return redirect("quiz", quiz_id=quiz.id)
+            # Create the quiz
+            quiz = Quiz.objects.create(
+                name=name,
+                description=description,
+                category=category,
+                created_by=request.user,
+                embedding=embedding,
+            )
+
+            return render(request, "quizly/create.html", {
+                "active_filter": "create",
+                "quiz": quiz,
+            })
+        
+        elif form_action == "save_question":
+            quiz_id = request.POST.get("quiz_id")
+            quiz = Quiz.objects.get(id=quiz_id)
+
+            question_text = request.POST.get("question_text")
+            hint = request.POST.get("hint")
+            option1 = request.POST.get("option1")
+            option2 = request.POST.get("option2")
+            option3 = request.POST.get("option3")
+            option4 = request.POST.get("option4")
+            solution = int(request.POST.get("solution"))
+
+            print(request.POST)
+
+
+            # Create the question
+            Question.objects.create(
+                text=question_text,
+                hint=hint,
+                option1=option1,
+                option2=option2,
+                option3=option3,
+                option4=option4,
+                solution=solution,
+                quiz=quiz,
+            )
+
+            return render(request, "quizly/create.html", {
+                "active_filter": "create",
+                "quiz": quiz,
+            })
+
+        #return redirect("quiz", quiz_id=quiz.id)
 
     else:
         categories = Category.objects.all()
